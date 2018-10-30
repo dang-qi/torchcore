746bd536e5420b3d429b215dea3dc4b1879e85d0
Heydar Maboudi Afkham <heydar@Nami.local>
2018-10-30 08:58:30 +0100

    Minor Updates

--- dnn/extensions/_init.py
+++ dnn/extensions/_init.py
@@ -14,13 +14,14 @@ if platform.system() == 'Darwin' :
     package_name = 'lib.macosx-%s-%s-%s' % ( mac_ver, machine, python_ver )
 
     libpath = os.path.join( dirpath, 'build', package_name )
+    print( libpath )
 
     if libpath not in sys.path :
         sys.path.append( libpath )
 elif platform.system() == "Linux" :
     filepath = os.path.realpath(__file__)
     dirpath = os.path.dirname(filepath)
-    
+
     python_ver = '.'.join( [ str(sys.version_info.major), str(sys.version_info.minor) ] )
     machine = platform.machine()
     package_name = 'lib.linux-%s-%s' % ( machine, python_ver )

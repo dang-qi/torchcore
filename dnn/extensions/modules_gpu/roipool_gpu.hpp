d0ba1f72e7a09b291371ac8255f55779f9de3ea6
Heydar Maboudi Afkham <heydar@Nami.local>
2018-12-21 21:54:08 +0100

    Minor Updates

--- dnn/extensions/modules_gpu/roipool_gpu.hpp
+++ dnn/extensions/modules_gpu/roipool_gpu.hpp
@@ -5,8 +5,6 @@
 #include <cfloat>
 #include "cuda_tools.hpp"
 
-using namespace std;
-
 void RoiPoolForwardGpu( const float* data, const int batch_size, const int nchannels, const int height, const int width,
                         const float* rois, const int* roibatches, const int nrois, const float spatial_scale,
                         float* crop, int* argmax, const int crop_height, const int crop_width );

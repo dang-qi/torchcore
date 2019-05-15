762cbcf8f096df87c25d4e3e4d7cb171ddf9dc91
Heydar Maboudi Afkham <heydar@gmail.com>
2019-05-15 23:21:34 +0200

    Added nms cpu

--- dnn/modules/setup.py
+++ dnn/modules/setup.py
@@ -6,6 +6,7 @@ def get_modules():
     modules = []
     modules.append( CppExtension('roi_pool_cpu', ['roi_pool_cpu.cpp']))
     modules.append( CppExtension('roi_align_cpu', ['roi_align_cpu.cpp']))
+    modules.append( CppExtension('nms_cpu', ['nms_cpu.cpp']))
     if torch.cuda.is_available() :
         modules.append(CUDAExtension('roi_pool_cuda', ['roi_pool_cuda.cpp','roi_pool_kernel.cu']))
         modules.append(CUDAExtension('roi_align_cuda', ['roi_align_cuda.cpp','roi_align_kernel.cu']))
---
+++ dnn/networks/tools/nms.py
@@ -0,0 +1,69 @@
+import torch
+import numpy as np
+from torch.nn import Module
+from torch.autograd import Function
+import nms_cpu
+
+def py_cpu_nms(dets, scores, thresh):
+    """Pure Python NMS baseline."""
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    #scores = dets[:, 4]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+    # sort the detections according to scores, high to low values.
+
+    keep = []
+
+    # keep doing until all bboxes are covered by THIS BBOX
+    while order.size > 0:
+        # choose the bbox with highest iou score, namely THIS BBOX
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+
+        # filter out all detections which have IOUs larger than the threshold, which means they are covered by THIS BBOX
+        inds = np.where(ovr <= thresh)[0]
+        order = order[inds + 1]
+
+    return np.array(keep)
+
+class NmsFunction( Function ):
+    @staticmethod
+    def forward( self, rois, scores, threshold, max_size ):
+        device = rois.device
+
+        rois = rois.cpu()#.detach().numpy()
+        scores = scores.cpu()#.detach().numpy()
+        #order = torch.argsort( scores, True )
+
+        nms_keep = nms_cpu.forward_cpu( rois, scores, threshold )
+        #nms_keep = py_cpu_nms( rois, scores, threshold )
+        nms_keep = nms_keep[:max_size]#.astype( np.int64 )
+        #nms_keep = torch.from_numpy( nms_keep ).to( device )
+
+        return nms_keep
+
+    @staticmethod
+    def backward( self, outgrad ):
+        return None, None, None
+
+class Nms( Module ):
+    def __init__( self, threshold, max_size ):
+        super().__init__()
+        self._threshold = threshold
+        self._max_size = max_size
+
+    def forward( self, rois, scores ):
+        return NmsFunction.apply(rois, scores, self._threshold, self._max_size)

d0816973ec71a5be33d5ee14270e55b234f37b0b
Heydar Maboudi Afkham <heydar@gmail.com>
2019-05-25 18:58:02 +0200

    Minor Updates

--- tools/torch_tools.py
+++ tools/torch_tools.py
@@ -1,6 +1,22 @@
 import os
 import torch
 
+def layer_init( m ):
+    if isinstance(m, nn.Conv2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.BatchNorm2d):
+        nn.init.constant_(m.weight, 1)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.Linear):
+        nn.init.normal_(m.weight, 0, 0.01)
+        nn.init.constant_(m.bias, 0)
+    elif isinstance(m, nn.ConvTranspose2d):
+        nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.constant_(m.bias, 0)
+
 def get_device( gpu ):
     if gpu is not None :
         os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152

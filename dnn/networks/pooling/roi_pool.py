915481bcf231b24a21fa1f7582f5d9eed5a010b5
Heydar Maboudi Afkham <heydar@gmail.com>
2019-06-02 22:52:24 +0200

    Minor Updates

--- dnn/networks/pooling/roi_pool.py
+++ dnn/networks/pooling/roi_pool.py
@@ -10,9 +10,5 @@ class ROIPool(Module):
 
     # feat: BxCxHxW,  rois: Kx4 (batch_idx, xmin, ymin, xmax, ymax) without normalize
     def forward(self, feat, rois, roibatches):
-        roibatches = roibatches.detach().cpu().numpy()
-        roibatches = roibatches.astype( np.float32 )
-        roibatches = torch.from_numpy( roibatches ).to( feat.device )
-
         rr = torch.cat([roibatches,rois], dim=1 )
         return self.op( feat, rr )

1147f7b00cccde7119504297db97eb1c21b5d053
Heydar Maboudi Afkham <heydar@gmail.com>
2019-06-02 22:31:59 +0200

    Minor Updates

--- dnn/networks/tools/nms.py
+++ dnn/networks/tools/nms.py
@@ -4,9 +4,16 @@ from torch.nn import Module
 from torchvision.ops import nms
 
 class Nms( Module ):
-    def __init__( self, threshold ):
+    def __init__( self, threshold, max ):
         super().__init__()
         self._threshold = threshold
+        self._max = max
 
     def forward( self, rois, scores ):
+        keep = nms( rois, scores, self._threshold )
+        print( scores[keep] )
+
+        input()
+
+
         return nms(rois, scores, self._threshold)

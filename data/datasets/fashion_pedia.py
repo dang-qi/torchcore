aab2a4adebd5bf3fc7f68f166de88f25c75228f0
Qi Dang <qida0163@dsv.su.se>
2021-04-09 12:14:45 +0200

    add fashion pedia dataset

---
+++ data/datasets/fashion_pedia.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+from .dataset_new import Dataset
+from PIL import Image
+import os
+
+class FashionPediaDataset(Dataset):
+    '''FashionPedia dataset'''
+    def __init__( self, root, anno, part, transforms=None, xyxy=True, debug=False ):
+        super().__init__( root, transforms )
+        self._part = part
+        folder_dict = {'train':'train', 'val':'test', 'test':'test'}
+        self._folder = folder_dict[part]
+
+        # load annotations
+        with open(anno, 'rb') as f:
+            self._images = pickle.load(f)[part] 
+        self.xyxy = xyxy
+        if xyxy:
+            self.convert_to_xyxy()
+        self.debug = debug
+
+    def __len__(self):
+        return len(self._images)
+
+    def __getitem__(self, idx):
+        image = self._images[idx]
+
+        # Load image
+        img_path = os.path.join(self._root, self._folder, image['file_name'] )
+        image_id=image['id']
+        img = Image.open(img_path).convert('RGB')
+        ori_image = img
+
+        # Load targets
+        boxes = []
+        labels = []
+        for obj in image['objects']:
+            boxes.append(obj['bbox'])
+            labels.append(obj['category_id'])
+        boxes = np.array(boxes, dtype=np.float32)
+        labels = np.array(labels, dtype=np.int64)
+
+        inputs = {}
+        inputs['data'] = img
+        if self.debug:
+            inputs['ori_image'] = ori_image
+
+        targets = {}
+        targets["boxes"] = boxes
+        targets["cat_labels"] = labels
+        targets["labels"] = labels
+        #target["masks"] = masks
+        targets["image_id"] = image_id
+        #target["area"] = area
+        #target["iscrowd"] = iscrowd
+        if self._transforms is not None:
+            inputs, targets = self._transforms(inputs, targets)
+
+        return inputs, targets
+
+    def convert_to_xyxy(self):
+        for image in self._images:
+            for obj in image['objects']:
+                obj['bbox'][2]+=obj['bbox'][0]
+                obj['bbox'][3]+=obj['bbox'][1]

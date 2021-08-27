ab4b7664aa6df66265fdeea5e3942a190dfba6c5
Qi Dang <qida0163@dsv.su.se>
2021-08-27 09:59:08 +0200

    add freeze bn

--- dnn/networks/general_detector.py
+++ dnn/networks/general_detector.py
@@ -13,3 +13,9 @@ class GeneralDetector(nn.Module):
 
     def post_process(self):
         raise NotImplementedError
+
+    def freeze_bn(self):
+        '''Freeze BatchNorm layers.'''
+        for layer in self.modules():
+            if isinstance(layer, nn.BatchNorm2d):
+                layer.eval()

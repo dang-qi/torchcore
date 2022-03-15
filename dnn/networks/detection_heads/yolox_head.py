8bb7440dc0c7293de3ea9c53c51385461371ec38
Qi Dang <qida0163@dsv.su.se>
2022-03-16 00:01:32 +0100

    minus change

--- dnn/networks/detection_heads/yolox_head.py
+++ dnn/networks/detection_heads/yolox_head.py
@@ -63,8 +63,10 @@ class YOLOXHead(BaseModule):
             pred_cls, pred_box, pred_obj = pred
             # flatten the preds NxCxHxW to NxH*WxC
             pred_box = pred_box.permute(0,2,3,1).reshape(N,-1,4)
-            pred_cls = pred_cls.permute(0,2,3,1).reshape(N,-1,C).sigmoid()
-            pred_obj = pred_obj.permute(0,2,3,1).reshape(N,-1).sigmoid()
+            #pred_cls = pred_cls.permute(0,2,3,1).reshape(N,-1,C).sigmoid()
+            #pred_obj = pred_obj.permute(0,2,3,1).reshape(N,-1).sigmoid()
+            pred_cls = pred_cls.permute(0,2,3,1).reshape(N,-1,C)
+            pred_obj = pred_obj.permute(0,2,3,1).reshape(N,-1)
             cat_pred_box.append(pred_box)
             cat_pred_cls.append(pred_cls)
             cat_pred_obj.append(pred_obj)
@@ -81,13 +83,16 @@ class YOLOXHead(BaseModule):
         for i in range(N):
             gt_boxes = targets[i]['boxes']
             gt_labels = targets[i]['labels']
-            pos_ind, cls_targets, obj_targets,box_targets, l1_targets =self.get_single_target(cat_pred_cls[i].detach(),cat_pred_obj[i].detach(),cat_pred_box[i].detach(),cat_decode_boxes[i].detach(), cat_priors_with_strides, gt_boxes, gt_labels)
-            return pos_ind, cls_targets, obj_targets,box_targets, l1_targets 
+            x =self.get_single_target(cat_pred_cls[i].detach(),cat_pred_obj[i].detach(),cat_pred_box[i].detach(),cat_decode_boxes[i].detach(), cat_priors_with_strides, gt_boxes, gt_labels)
+            return x
+            #pos_ind, cls_targets, obj_targets,box_targets, l1_targets =self.get_single_target(cat_pred_cls[i].detach(),cat_pred_obj[i].detach(),cat_pred_box[i].detach(),cat_decode_boxes[i].detach(), cat_priors_with_strides, gt_boxes, gt_labels)
+            #return pos_ind, cls_targets, obj_targets,box_targets, l1_targets 
 
     @torch.no_grad()
     def get_single_target(self, pred_cls, pred_obj, pred_box, decode_boxes, prior_with_strides, gt_boxes, gt_labels):
         '''get pred targets for each image'''
         # all the pred are concated predictions from all the FPN level
+        #return (pred_cls, pred_obj,pred_box,decode_boxes,prior_with_strides,gt_boxes,gt_labels)
         num_prior = prior_with_strides.size(0)
         gt_num = gt_boxes.size(0)
         # if there is no gt boxes
@@ -101,20 +106,20 @@ class YOLOXHead(BaseModule):
 
         offset_priors_with_strides = torch.cat([prior_with_strides[:,:2]+0.5*prior_with_strides[:,2:], prior_with_strides[:,2:]], dim=-1)
 
-        match = self.box_matcher.match(pred_cls*pred_obj.unsqueeze(-1), offset_priors_with_strides, decode_boxes, gt_boxes, gt_labels )
+        match = self.box_matcher.match(pred_cls.sigmoid()*pred_obj.sigmoid().unsqueeze(-1), offset_priors_with_strides, decode_boxes, gt_boxes, gt_labels )
         pos_ind = match.matched_ind>=0
         pos_num = pos_ind.sum()
 
         pos_ious = match.max_iou[pos_ind]
         # class targets are IoU awear 
-        cls_targets = F.one_hot(match.matched_ind[pos_ind],num_classes=self.num_classes) * pos_ious.unsqueeze(-1)
+        cls_targets = F.one_hot(gt_labels[match.matched_ind[pos_ind]],num_classes=self.num_classes) * pos_ious.unsqueeze(-1)
         obj_targets = pred_obj.new_zeros((num_prior,1))
         obj_targets[pos_ind] = 1
         box_targets = gt_boxes[match.matched_ind[pos_ind]]
         l1_targets = pred_box.new_zeros((pos_num,4))
         if self.use_l1:
             l1_targets = self._get_l1_targets(box_targets,l1_targets,prior_with_strides[pos_ind])
-        return pos_ind, cls_targets, obj_targets,box_targets, l1_targets 
+        return (pos_ind, cls_targets, obj_targets,box_targets, l1_targets)
 
     def _get_l1_targets(self,box_targets,l1_targets,priors,eps=1e-8):
         '''(xc-Pxc)/w, (yc-Pyc)/h, log(w/stride_w),log(h/stride_h)
@@ -122,7 +127,8 @@ class YOLOXHead(BaseModule):
         '''
         box_targets_cxcywh = xyxy2cxcywh(box_targets)
         l1_targets[:,:2] = (box_targets_cxcywh[:,:2]-priors[:,:2])/priors[:,2:]
-        l1_targets[:,2:] = -torch.log(box_targets_cxcywh[:,2:]/priors[:,2:]+eps)
+        l1_targets[:,2:] = torch.log(box_targets_cxcywh[:,2:]/priors[:,2:]+eps)
+        return l1_targets
 
 
     def decode_boxes(self, pred, prior, stride):
